8d0ac11838174e66ec3e936fffe15a0d51ef5508
Leonardo Marinho <leonardo.marinho@gmail.com>
2019-05-22 11:39:38 -0300

    Bugfix for running project from root folder

--- src/index.js
+++ src/index.js
@@ -42,5 +42,5 @@ const resolvers = {
   }
 }
 
-const server = new GraphQLServer({typeDefs: "schema.graphql", resolvers})
+const server = new GraphQLServer({typeDefs: "./src/schema.graphql", resolvers})
 server.start(() => console.log(`Server is running on http://localhost:4000`))
